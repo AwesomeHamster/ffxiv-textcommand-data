02cfc5c441d2c271d60ba85d4af0e065877310c2
Maiko Tan <maiko.tan.coding@gmail.com>
2022-12-10 22:18:14 +0800

    build: fix build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,6 +1,6 @@
 import { build, BuildOptions } from 'esbuild'
 import { sync as glob } from 'fast-glob'
-import { writeFileSync } from 'fs'
+import { writeFile } from 'fs/promises'
 import { transpile } from 'typescript'
 
 const options: BuildOptions = {
@@ -10,20 +10,22 @@ const options: BuildOptions = {
   sourcemap: 'linked',
 }
 
-console.log('Building CommonJS module...')
-build({
-  ...options,
-  target: 'node12',
-  format: 'cjs',
-  outdir: './dist',
-})
-
-console.log('Building d.ts files...')
-transpile('src/index.ts', {
-  declaration: true,
-  emitDeclarationOnly: true,
-  outDir: 'dist',
-})
-
-console.log('Adding ESM entry...')
-writeFileSync('dist/index.mjs', `export * from './index.js'`)
+;(async () => {
+  console.log('Building CommonJS module...')
+  await build({
+    ...options,
+    target: 'node12',
+    format: 'cjs',
+    outdir: 'dist',
+  })
+  
+  console.log('Building d.ts files...')
+  await transpile('src/index.ts', {
+    declaration: true,
+    emitDeclarationOnly: true,
+    outDir: 'dist',
+  })
+  
+  console.log('Adding ESM entry...')
+  await writeFile('dist/index.mjs', `export * from './index.js'`)
+})()
